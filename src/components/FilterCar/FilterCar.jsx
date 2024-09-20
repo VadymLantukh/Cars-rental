a4378c22acd05a9915c30b822a61681cfc2c9da8
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-09-20 11:36:18 +0300

    Update FilterCar

--- src/components/FilterCar/FilterCar.jsx
+++ src/components/FilterCar/FilterCar.jsx
@@ -10,22 +10,22 @@ import { selectFilterCars } from "../../redux/cars/selectors";
 import toast from "react-hot-toast";
 
 const FilterCar = () => {
-  const [hasSearched, setHasSearched] = useState(false);
+  const [isSearch, setIsSearch] = useState(false);
   const dispatch = useDispatch();
   const cars = useSelector(selectFilterCars);
   const [brand, setBrand] = useState(null);
   const [price, setPrice] = useState(null);
 
   useEffect(() => {
-    if (hasSearched && cars.length === 0) {
+    if (isSearch && cars.length === 0) {
       toast.error(
         "No cars were found according to your search, a possible reason is that you have not yet uploaded car card, using the Load more button"
       );
     }
-  }, [cars, hasSearched]);
+  }, [cars, isSearch]);
 
   const handleFilter = () => {
-    setHasSearched(true);
+    setIsSearch(true);
     dispatch(filterCars({ brand: brand?.value, price: price?.value }));
   };
 
