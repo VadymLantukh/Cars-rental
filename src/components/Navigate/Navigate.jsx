9a5290857946393b8e7a669912648ce579ecd8ce
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-09-24 08:15:32 +0300

    add active class for navigate, add for imgCar style css,

--- src/components/Navigate/Navigate.jsx
+++ src/components/Navigate/Navigate.jsx
@@ -2,15 +2,17 @@ import { NavLink } from "react-router-dom";
 import css from "./Navigate.module.css";
 
 const Navigate = () => {
+  const addActive = ({ isActive }) => (isActive ? css.active : css.inactive);
+
   return (
     <nav className={css.navBox}>
-      <NavLink className={css.navLink} to="/">
+      <NavLink className={addActive} to="/">
         Home
       </NavLink>
-      <NavLink className={css.navLink} to="/catalog">
+      <NavLink className={addActive} to="/catalog">
         Catalog
       </NavLink>
-      <NavLink className={css.navLink} to="/favorites">
+      <NavLink className={addActive} to="/favorites">
         Favorites
       </NavLink>
     </nav>
